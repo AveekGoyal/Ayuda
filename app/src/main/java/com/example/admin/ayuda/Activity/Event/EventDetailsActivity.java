eb6183a75c1eed727afa8f6d86bf447135f35b13
orignallazycoder <technoarpitorignal@gmail.com>
2018-03-14 00:09:17 +0530

    Added Java to spinners And Date time picker in: AddEventActivity.java EventDetailActivity.java NgoRegistrationActivity.java
    and Layouts completed:
    add_event_Activity.xml
    event_details.activity.xml
    nog_registration_activity.xml

--- app/src/main/java/com/example/admin/ayuda/Activity/Event/EventDetailsActivity.java
+++ app/src/main/java/com/example/admin/ayuda/Activity/Event/EventDetailsActivity.java
@@ -3,13 +3,25 @@ package com.example.admin.ayuda.Activity.Event;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.admin.ayuda.R;
 
 public class EventDetailsActivity extends AppCompatActivity {
+
+    private Spinner EventDetailGoingSpinner;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_details_activity);
+
+        //Connecting Var. with IDs
+        EventDetailGoingSpinner = (Spinner) findViewById(R.id.EventDetailGoingSpinner);
+
+        //For Going waala spinner
+        String[] status = {"Intrested" , "Going" , "Not Going"};
+        ArrayAdapter<String> adapterStatus = new ArrayAdapter<String>(this , android.R.layout.simple_spinner_dropdown_item , status);
+        EventDetailGoingSpinner.setAdapter(adapterStatus);
     }
 }
