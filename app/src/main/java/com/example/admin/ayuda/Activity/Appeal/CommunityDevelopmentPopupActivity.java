1741914d3d5927b32a4343fd0fa6ce2cfe6d0ff2
orignallazycoder <technoarpitorignal@gmail.com>
2018-03-14 17:36:52 +0530

    Community Development Iterfaces done with basic Java

--- app/src/main/java/com/example/admin/ayuda/Activity/Appeal/CommunityDevelopmentPopupActivity.java
+++ app/src/main/java/com/example/admin/ayuda/Activity/Appeal/CommunityDevelopmentPopupActivity.java
@@ -4,13 +4,21 @@ package com.example.admin.ayuda.Activity.Appeal;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.example.admin.ayuda.R;
 
 public class CommunityDevelopmentPopupActivity extends AppCompatActivity {
+
+    private Button communityDevPopupProceedButton;
+    private Button communityDevPopupRejectButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.community_development_popup);
+
+        communityDevPopupProceedButton = findViewById(R.id.CommunityDevPopupProceedButton);
+        communityDevPopupRejectButton = findViewById(R.id.CommunityDevPopupRejectButton);
     }
 }
