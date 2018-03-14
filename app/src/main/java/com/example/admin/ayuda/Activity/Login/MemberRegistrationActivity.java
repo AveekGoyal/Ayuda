00b9f713a0ae765f22797796de44d5ed773fed29
orignallazycoder <technoarpitorignal@gmail.com>
2018-03-14 15:49:21 +0530

    Event Module and Login Module Completed with card View and Java Work.
    Recycler View

--- app/src/main/java/com/example/admin/ayuda/Activity/Login/MemberRegistrationActivity.java
+++ app/src/main/java/com/example/admin/ayuda/Activity/Login/MemberRegistrationActivity.java
@@ -2,17 +2,57 @@ package com.example.admin.ayuda.Activity.Login;
 
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import com.example.admin.ayuda.R;
 
 public class MemberRegistrationActivity extends AppCompatActivity{
 
+    private ImageButton memRegDpImageButton;
+    private EditText memRegFirstNameTextBox;
+    private EditText memRegLastNameTextBox;
+    private RadioButton memRegRadioMale;
+    private RadioButton memRegRadioFemale;
+    private  EditText memRegEmailTextBox;
+    private  EditText memRegPasswordTextBox;
+    private  EditText memRegConfirmPassTextBox;
+    private  EditText memRegMobileNoTextBox;
+    private Spinner memRegOrgNameSpinner;
+    private EditText memRegMemberIdTextBox;
+    private EditText memRegAddressTextBox;
+    private  Spinner memRegCitySpinner;
+    private Spinner memRegStateSpinner;
+    private EditText memRegPincodeTextBox;
+    private Button memRegSubmitButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_registration_activity);
+
+        memRegDpImageButton = findViewById(R.id.MemRegDpImageButton);
+        memRegFirstNameTextBox= findViewById(R.id.MemRegFirstNameTextBox);
+        memRegLastNameTextBox = findViewById(R.id.MemRegLastNameTextBox);
+        memRegRadioMale = findViewById(R.id.MemRegRadioMale);
+        memRegRadioFemale = findViewById(R.id.MemRegRadioFemale);
+        memRegEmailTextBox = findViewById(R.id.MemRegEmailTextBox);
+        memRegPasswordTextBox =findViewById(R.id.MemRegPasswordTextBox);
+        memRegConfirmPassTextBox = findViewById(R.id.MemRegConfirmPassTextBox);
+        memRegMobileNoTextBox =findViewById(R.id.MemRegMobileNoTextBox);
+        memRegOrgNameSpinner = findViewById(R.id.MemRegOrgNameSpinner);
+        memRegMemberIdTextBox =findViewById(R.id.MemRegMemberIdTextBox);
+        memRegAddressTextBox = findViewById(R.id.MemRegAddressTextBox);
+        memRegCitySpinner = findViewById(R.id.MemRegCitySpinner);
+        memRegStateSpinner = findViewById(R.id.MemRegStateSpinner);
+        memRegSubmitButton = findViewById(R.id.MemRegSubmitButton);
+        memRegPincodeTextBox = findViewById(R.id.MemRegPincodeTextBox);
+
     }
 }
--- app/src/main/java/com/example/admin/ayuda/Activity/Login/NonMemberRegistrationActivity.java
+++ app/src/main/java/com/example/admin/ayuda/Activity/Login/NonMemberRegistrationActivity.java
@@ -1,15 +1,47 @@
 package com.example.admin.ayuda.Activity.Login;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import com.example.admin.ayuda.R;
 
-
 public class NonMemberRegistrationActivity extends AppCompatActivity {
+
+    private ImageButton nonMemRegDpImageButton;
+    private EditText nonMemRegFirstNameTextBox;
+    private EditText nonMemRegLastNameTextBox;
+    private RadioButton nonMemRegRadioMale;
+    private EditText nonMemRegContactNoTextBox;
+    private  RadioButton nonMemRegRadioFemale;
+    private  EditText nonMemRegEmailTextBox;
+    private  EditText nonMemRegPasswordTextBox;
+    private  EditText nonMemRegConfirmPassTextBox;
+    private Button nonMemberSubmitButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.non_member_registration_activity);
+
+        //Variable used in this java file
+        nonMemRegDpImageButton = findViewById(R.id.NonMemRegDpImageButton);
+        nonMemRegFirstNameTextBox = findViewById(R.id.NonMemRegFirstNameTextBox);
+        nonMemRegLastNameTextBox = findViewById(R.id.NonMemRegLastNameTextBox);
+        nonMemRegRadioMale = findViewById(R.id.NonMemRegRadioMale);
+        nonMemRegRadioFemale = findViewById(R.id.NonMemRegRadioFemale);
+        nonMemRegContactNoTextBox = findViewById(R.id.NonMemRegContactNoTextBox);
+        nonMemRegEmailTextBox = findViewById(R.id.NonMemRegEmailTextBox);
+        nonMemRegPasswordTextBox = findViewById(R.id.NonMemRegPasswordTextBox);
+        nonMemRegConfirmPassTextBox = findViewById(R.id.NonMemRegConfirmPassTextBox);
+        nonMemberSubmitButton = findViewById(R.id.NonMemberSubmitButton);
+
+
     }
 }
