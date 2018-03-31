1dede7882752bec764a088cc087034b689ad186e
orignallazycoder <technoarpitorignal@gmail.com>
2018-03-31 11:44:57 +0530

    Added Stats (Do not Use Them) validate_Member is converted to verify appeals completion

---
+++ app/src/main/java/com/example/admin/ayuda/Activity/NgoStatistics.java
@@ -0,0 +1,15 @@
+package com.example.admin.ayuda.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.ayuda.R;
+
+public class NgoStatistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngo_statistics);
+    }
+}
